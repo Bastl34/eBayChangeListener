b420ef019f239eeea3176d3aa275e9ce54c2a961
Bastian Karge <bastian.karge@gmail.com>
2016-11-07 21:20:45 +0100

    - ignore interval changed

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config =
 	//check for new items every:
 	timerInterval: (30*1000),
 	//ignore all items older than:
-	ignoreInterval: (24*60*60*1000)
+	ignoreInterval: (20*60*1000)
 };
 
 module.exports = config;
